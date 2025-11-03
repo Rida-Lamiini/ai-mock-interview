7ae245e99f502c9d744ec35201d4306f8f646fc4
Rida-Lamiini <ridalamiini0@gmail.com>
2025-11-03 15:14:17 +0000

    Fix CORS allowed origins and update Dockerfile for nginx configuration

--- backend/src/main/java/com/lamiini/backend/config/WebConfig.java
+++ backend/src/main/java/com/lamiini/backend/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000/") // Adjust this according to your frontend URL
+                        .allowedOrigins("http://localhost:3000") // Adjust this according to your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
